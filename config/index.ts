503ab8f9f66630bd5134e7345cb48d119ee69e64
Jerryqjr <1317102375@qq.com>
2024-07-19 16:16:02 +0800

    修改title,description

--- config/index.ts
+++ config/index.ts
@@ -3,8 +3,8 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
+  title: '华通人',
+  description: '问答华通云公司相关数据的机器人',
   copyright: '',
   privacy_policy: '',
   default_language: 'zh-Hans',

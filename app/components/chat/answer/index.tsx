d57a7cc7103d8b1bdb4a6f095e11e4531ce8f735
Jerryqjr <1317102375@qq.com>
2024-07-25 10:32:57 +0800

    调整美女图标大小

--- app/components/chat/answer/index.tsx
+++ app/components/chat/answer/index.tsx
@@ -155,7 +155,7 @@ const Answer: FC<IAnswerProps> = ({
   return (
     <div key={id}>
       <div className='flex items-start'>
-        <div className={`${s.answerIcon} w-7 h-7 shrink-0 rounded-full`}>
+        <div className={`${s.answerIcon} w-10 h-10 shrink-0 `}>
           {isResponsing
             && <div className={s.typeingIcon}>
               <LoadingAnim type='avatar' />
